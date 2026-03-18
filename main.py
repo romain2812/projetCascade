9e9f4927261e4bd01d61815c1ded3bfe8d5c7ff6
Romain Pauly <romainpauly@icloud.com>
2026-03-18 22:50:45 +0100

    jsp 3

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 
+def afficher_grille(grille):
+    print("______________________________")
+    for i in range(len(grille)):
+        for j in range(len(grille[0])):
+            if j == len(grille[0])-1:
+                print(f"|{grille[i][j]}|")
+            else:
+                print(f"|{grille[i][j]}", end=" ")
+
+
+
 
 
 def depart():
@@ -8,6 +19,7 @@ def depart():
               [" "," "," "," "," "],
               ["O"," "," "," ","O"],
               ["O","O","O","O","O"]]
+    afficher_grille(grille)
     
 
 
@@ -18,34 +30,34 @@ def millieu():
               ["X"," "," "," "," "],
               ["O"," ","X"," ","X"],
               ["O","O","O"," ","O"]]
+    afficher_grille(grille)
 
 
 def fin():
-    return ...
-
-
-def main():
     auTourDe = "le joueur O"
     grille = [[" "," ","X","O","X"],
               ["X"," ","O"," ","O"],
               [" "," ","O","O","X"],
               [" ","O","X"," "," "],
               ["X"," "," ","O","X"]]
+    afficher_grille(grille)
+    
 
+def main():
 
     print("qu'elle situation de jeu voulais vous:")
     print("                             1/depart")
     print("                             2/le milieu")
     print("                             3/la fin")
     choix = input("saisier un numero: ")
-    match choix:
-        case 1:
-            depart()
-        case 2:
-            millieu()
-        case 3:
-            fin()
-
+    if choix == "1":
+        print("1")
+        depart()
+    elif choix =="2":
+        millieu()
+    else:
+        fin()
+    
     
 
 
