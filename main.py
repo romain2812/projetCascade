9b33dcd91e2a1e1bbb4eddc8257f7f9f245f47d7
Romain Pauly <romainpauly@icloud.com>
2026-03-19 18:05:53 +0100

    jsp 4

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 
-def afficher_grille(grille):
+ # -*- coding: utf-8 -*-
+
+def afficher_grille(grille,auTourDe):
     print("______________________________")
+    if auTourDe:
+        print("c'est au joueur au X de jouer\n")
+    else: 
+        print("c'est au joueur au O de jouer\n")
     print("    1  2  3  4  5")
+    convNbLettre ={0:'A',1:'B',2:'C',3:'D',4:'E'}
     for i in range(len(grille)):
-        print(i, end=" ")
+        print(convNbLettre[i], end=" ")
         for j in range(len(grille[0])):
             if j == len(grille[0])-1:
                 print(f"|{grille[i][j]} |")
@@ -21,34 +28,34 @@ def demanderUneCase():
 
 
 def depart():
-    auTourDe = "le joueur O"
+    auTourDe = 1 #quand c'est a True c'est au joueur au X et quand c'est a false au joueur au O 
     grille = [["X","X","X","X","X"],
               ["X"," "," "," ","X"],
               [" "," "," "," "," "],
               ["O"," "," "," ","O"],
               ["O","O","O","O","O"]]
-    afficher_grille(grille)
+    afficher_grille(grille,auTourDe)
     
 
 
 def millieu():
-    auTourDe = "le joueur O"
+    auTourDe = 1 #quand c'est a True c'est au joueur au X et quand c'est a false au joueur au O 
     grille = [[" ","X","O","X","X"],
               [" "," "," ","O","X"],
               ["X"," "," "," "," "],
               ["O"," ","X"," ","X"],
               ["O","O","O"," ","O"]]
-    afficher_grille(grille)
+    afficher_grille(grille,auTourDe)
 
 
 def fin():
-    auTourDe = "le joueur O"
+    auTourDe = True #quand c'est a True c'est au joueur au X et quand c'est a false au joueur au O 
     grille = [[" "," ","X","O","X"],
               ["X"," ","O"," ","O"],
               [" "," ","O","O","X"],
               [" ","O","X"," "," "],
               ["X"," "," ","O","X"]]
-    afficher_grille(grille)
+    afficher_grille(grille,auTourDe)
     
 
 def main():
@@ -58,15 +65,20 @@ def main():
     print("                             2/le milieu")
     print("                             3/la fin")
     choix = input("saisier un numero: ")
-    if choix == "1":
-        print("1")
-        depart()
-    elif choix =="2":
-        millieu()
-    else:
-        fin()
+    match choix:
+        case "1":
+            depart()
+        case "2":
+            millieu()
+        case "3":
+            fin()
     
-    
+def est_au_bon_format(message):
+    ...
+
+
+def est_dans_grille():
+    ...
 
 
 main()
